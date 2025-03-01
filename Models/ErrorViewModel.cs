142e7eee470af1720915bc2e69654a2b816c1301
nandan <parmar.nandan189@gmail.com>
2025-03-01 22:45:24 +0530

    Project Setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TheBookStore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
